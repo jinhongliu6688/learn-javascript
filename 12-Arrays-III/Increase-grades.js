08943983d9515d4574816b8a305794cb7c994681
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-01-17 14:10:26 -0800

    Create Increase-grades.js

---
+++ 12-Arrays-III/Increase-grades.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} grades
+ */
+const shouldAdjustGrades = grades => {
+    return grades.some(grade => grade < 10)
+}
+
+
+// Sample usage - do not modify
+shouldAdjustGrades([10, 12, 10, 14]); // false
+shouldAdjustGrades([12, 8, 17]); // true
