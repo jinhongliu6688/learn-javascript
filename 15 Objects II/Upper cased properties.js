29b63f4204a304ce990554eac1b22816c3fd51a9
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-01-17 22:58:08 -0800

    Create Upper cased properties.js

---
+++ 15 Objects II/Upper cased properties.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Object} course
+ * @param {number} [course.id]
+ * @param {string} [course.name]
+ * @param {number} [course.year]
+ */
+const getUpperCasedProperties = course => {
+    return Object.keys(course).map(key => {
+        return key.toUpperCase()
+    })
+}
+
+// Sample usage - do not modify
+console.log(getUpperCasedProperties({id: 1, name: "Learn JavaScript", year: 2021})); // ["ID", "NAME", "YEAR"]
+console.log(getUpperCasedProperties({name: "Learn JavaScript", category: "Programming"})); // ["NAME", "CATEGORY"]
+console.log(getUpperCasedProperties({})); // []
