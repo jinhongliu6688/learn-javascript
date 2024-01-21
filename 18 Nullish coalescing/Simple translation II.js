95aeb9ae4d07359fdd1e149161f606cad3b7f824
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-01-20 21:58:07 -0800

    Create Simple translation II.js

---
+++ 18 Nullish coalescing/Simple translation II.js
@@ -0,0 +1,10 @@
+import translations from "./translations.js";
+
+/**
+ * @param {string} language
+ */
+export const getTranslation = (language) => {
+    console.log(translations);
+    console.log(language);
+    return translations.welcome[language] ?? "Welcome"
+}
