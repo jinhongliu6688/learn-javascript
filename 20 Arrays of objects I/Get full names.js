dc3cef5e8e78287077ec1cca587f4ee3805d5590
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-01-23 00:18:22 -0800

    Create Get full names.js

---
+++ 20 Arrays of objects I/Get full names.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Object[]} users
+ */
+const getFullNames = users => {
+    return users.map(user => {
+        return (user.firstName + " " + user.lastName).toUpperCase()
+    })
+}
+
+// Sample usage - do not modify
+const users = [{
+    id: 1,
+    firstName: "Sam",
+    lastName: "Green"
+}, {
+    id: 2,
+    firstName: "Alex",
+    lastName: "Blue"
+}];
+console.log(getFullNames(users)); // ["SAM GREEN", "ALEX BLUE"]
