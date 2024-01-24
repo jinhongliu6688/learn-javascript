b2c4b29e900342603296303e2d0b373bc3d60e9c
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-01-23 18:29:00 -0800

    Create Sum message count.js

---
+++ 22 Reducing arrays of objects/Sum message count.js
@@ -0,0 +1,31 @@
+/**
+ * @param {Object[]} groups
+ * @param {number} groups.id
+ * @param {string} groups.title
+ * @param {object} groups.details
+ * @param {number} groups.details.messageCount
+ * @param {boolean} groups.details.public
+ */
+const sumMessageCount = groups => {
+    return groups.reduce((total, current) => {
+        return total + current.details.messageCount
+    }, 0)
+}
+
+// Sample usage - do not modify
+const sampleGroups = [{
+    id: 1,
+    title: "Football",
+    details: {
+        messageCount: 30,
+        public: true
+    }
+}, {
+    id: 2,
+    title: "Family",
+    details: {
+        messageCount: 1014,
+        public: false
+    }
+}];
+console.log(sumMessageCount(sampleGroups)); // 1044
