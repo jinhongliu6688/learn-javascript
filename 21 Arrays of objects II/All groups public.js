20d57b497ec0f3af8107edb0e3215f05c9e4bd5f
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-01-23 17:17:59 -0800

    Create All groups public.js

---
+++ 21 Arrays of objects II/All groups public.js
@@ -0,0 +1,31 @@
+/**
+ * @param {Object[]} groups
+ * @param {number} groups.id
+ * @param {string} groups.title
+ * @param {object} groups.details
+ * @param {number} groups.details.messageCount
+ * @param {boolean} groups.details.isPublic
+ */
+const allGroupsPublic = groups => {
+    return groups.every(group => {
+        return group.details.isPublic == true
+    })
+}
+
+// Sample usage - do not modify
+const sampleGroups = [{
+    id: 1,
+    title: "Football",
+    details: {
+        messageCount: 30,
+        isPublic: true
+    }
+}, {
+    id: 2,
+    title: "Family",
+    details: {
+        messageCount: 1014,
+        isPublic: false
+    }
+}];
+console.log(allGroupsPublic(sampleGroups));
