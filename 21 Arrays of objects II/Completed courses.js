df453ce474b226de54ae9345414f8d59e9bb43a4
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-01-23 17:08:07 -0800

    Create Completed courses.js

---
+++ 21 Arrays of objects II/Completed courses.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Object[]} courses
+ * @param {number} courses.id
+ * @param {string} courses.name
+ * @param {boolean} courses.isCompleted
+ */
+const getCompletedCourses = courses => {
+    return courses.filter(course => {
+        return course.isCompleted == true
+    })
+}
+
+// Sample usage - do not modify
+const sampleCourses = [{
+    id: 1,
+    name: "Learn Programming",
+    isCompleted: true
+}, {
+    id: 2,
+    name: "Learn JavaScript",
+    isCompleted: false
+}];
+console.log(getCompletedCourses(sampleCourses));
