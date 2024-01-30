a77eb9c8c0770680350c5a31382a6f6df4615773
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-01-30 00:25:21 -0800

    Create index.js

---
+++ 33. Public and private fields/Refactor book sale I/index.js
@@ -0,0 +1,25 @@
+class BookSale {
+    amount = 1_000; // US cents
+    currency = "usd";
+    isStudent = false;
+
+    applyStudentDiscount() {
+        this.isStudent = true;
+        this.amount = 800;
+        return this;
+    }
+
+    setCurrency(currency) {
+        this.currency = currency;
+        return this;
+    }
+
+    applyPercentageDiscount(percent) {
+        this.amount = this.amount - this.amount * percent / 100;
+        return this;
+    }
+}
+
+// Sample usage - do not modify
+const bookSale = new BookSale;
+bookSale.applyStudentDiscount().setCurrency("eur").applyPercentageDiscount(5);
