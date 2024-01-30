44e6acd8c775f2ae942b90468aa08ba9cd9870db
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-01-29 16:53:31 -0800

    Create Android & iOS.js

---
+++ 30 Class inheritance/Android & iOS.js
@@ -0,0 +1,31 @@
+// TODO: define classes here
+class MoblieSystem {
+    constructor(name, price) {
+        this.name = name
+        this.price = price
+    }
+
+    getDescription() {
+        return `The ${this.name} costs ${this.price}`
+    }
+}
+
+class Android extends MoblieSystem {
+    getVersion() {
+        return 12
+    }
+}
+
+class iOS extends MoblieSystem {
+    getVersion() {
+        return 15
+    }
+}
+
+// Sample usage - do not modify
+const galaxy = new Android("Galaxy", 400);
+console.log(galaxy.getDescription()); // "The Galaxy costs 400"
+console.log(galaxy.getVersion()); // 12 (always for android phones)
+const iphone = new iOS("iPhone", 500);
+console.log(iphone.getDescription()); // "The iPhone costs 500"
+console.log(iphone.getVersion()); // 15 (always for iOS phones)
