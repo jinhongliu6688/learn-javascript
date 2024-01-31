af132f0c2a388d55814f78a2727f2bb1f5c1efde
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-01-30 16:37:22 -0800

    Create index.js

---
+++ 35. Callback pattern/Callback pattern/index.js
@@ -0,0 +1,11 @@
+import {welcomeUser} from "./helpers.js";
+
+/**
+ * @param {string} name
+ */
+const sayHello = (name) => {
+    welcomeUser(name, () => console.log("Done!"))
+}
+
+// Sample usage - do not modify
+sayHello("Sam");
