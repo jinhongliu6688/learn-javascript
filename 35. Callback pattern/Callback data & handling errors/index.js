f35c4c47f4b144b4e146093ce0a578edfc1c7cac
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-01-30 17:21:23 -0800

    Create index.js

---
+++ 35. Callback pattern/Callback data & handling errors/index.js
@@ -0,0 +1,12 @@
+import {sumGrades} from "./helpers.js";
+
+/**
+ * @param {number[]} grades
+ */
+const calculateSum = (grades) => {
+    sumGrades(grades, value => console.log(`The sum is: ${value}`));
+}
+
+// Sample usage - do not modify
+calculateSum([18, 10]);
+calculateSum([15, 10, 20]);
