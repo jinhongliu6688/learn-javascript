793a112645456c18118eaec56b8a05a7dd31acc1
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-04 20:37:32 -0800

    Create index.js

---
+++ 42. Intro to fetch/Get new notifications I/index.js
@@ -0,0 +1,8 @@
+const checkForNewNotifications = () => {
+    return fetch("https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/notifications/new.json")
+}
+
+
+// Sample usage - do not modify
+const result = checkForNewNotifications();
+console.log(result); // Promise{<pending>}
