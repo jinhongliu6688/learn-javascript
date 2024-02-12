52ff7ffc4950c87e4debff31b4f11295a8144f7a
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-11 22:34:29 -0800

    Create index.js

---
+++ 48. DOM Selection I/Selectors/index.js
@@ -0,0 +1,26 @@
+const getNavbarElement = () => {
+    // TODO: get the element with id navbar
+    return document.querySelector("#navbar")
+}
+
+const getMainElement = () => {
+    // TODO: get the element with id main
+    return document.querySelector("#main")
+}
+
+const getAboutFromFooter = () => {
+    // TODO: get the about link that's in the footer
+    return document.querySelector("footer .about")
+}
+
+const getTheParagraphElement = () => {
+    // TODO: get the first paragraph element
+    return document.querySelector("p")
+}
+
+
+// Sample usage - do not modify
+console.log(getNavbarElement());
+console.log(getMainElement());
+console.log(getAboutFromFooter());
+console.log(getTheParagraphElement());
