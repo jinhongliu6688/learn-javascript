128a62b05cf9ee8ccb201d2413a9962509699a79
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-13 00:00:07 -0800

    Create index.js

---
+++ 50. DOM innerHTML and value/Set message (not trusted)/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} name
+ */
+const setWelcomeMessage = name => {
+    document.querySelector("#name").textContent = name
+}
+
+// Sample usage - do not modify
+setWelcomeMessage("Sam");
+setWelcomeMessage("<h2>You have been hacked</h2>");
