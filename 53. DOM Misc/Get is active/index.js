2f76f2cdbbe6b180c997a17911980c9c0dbb1e38
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-14 19:44:04 -0800

    Create index.js

---
+++ 53. DOM Misc/Get is active/index.js
@@ -0,0 +1,7 @@
+const getIsActiveFromCard = () => {
+    return document.querySelector("#user-card").dataset.isActive === "true"
+
+}
+
+// Sample usage - do not modify
+console.log(getIsActiveFromCard());
