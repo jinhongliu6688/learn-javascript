ac72391cc8ec5fd41c580b092754f2d755d1b2de
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-17 02:54:34 -0800

    Create index.js

---
+++ 58. DOM Events III/Instant search project/index.js
@@ -0,0 +1,26 @@
+import {data} from "./data.js";
+
+// visualize one of the items in `data` (feel free to remove the next line)
+console.log(data[0]);
+
+// TODO: implement instant search
+const list = document.querySelector("#results-list")
+const inputForm = document.querySelector("#app-search")
+
+data.forEach(item => {
+    list.insertAdjacentHTML("afterbegin", `<li>${item.spacecraft}</li>`);
+})
+
+const search = document.querySelector("#app-search")
+search.addEventListener("keyup", event => {
+    if (event.key !== "") {
+        list.innerHTML = ""
+        const input_low = inputForm.value.toLowerCase().trim()
+        console.log(input_low)
+        data.forEach(item => {
+            if (item.spacecraft.toLowerCase().includes(input_low)) {
+                list.insertAdjacentHTML("beforeend", `<li>${item.spacecraft}</li>`);
+            }
+        })
+    }
+})
