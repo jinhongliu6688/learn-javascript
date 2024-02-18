6a83888c4f1edbca9c31467fc7f84cefd3294ff3
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-17 21:25:31 -0800

    Update index.js

--- 59. DOM + fetch/Todo App API Project/index.js
+++ 59. DOM + fetch/Todo App API Project/index.js
@@ -4,27 +4,40 @@ import FetchWrapper from "./fetch-wrapper.js"
 const API = new FetchWrapper("https://api.learnjavascript.online/demo/")
 const todoList = document.querySelector("#todos-list")
 
-const showTodos = () => {
+const showRecentTodos = () => {
     API.get("todos").then(data => {
+        todoList.innerHTML = ""
         data.todos.forEach(item => {
             todoList.insertAdjacentHTML("beforeend", `<li><div class="card">[${item.category}] ${item.title}</div></li>`)
         })
     })
 }
 
-showTodos()
+// showRecentTodos()
 
 const form = document.querySelector("#todo-form")
 const todo = document.querySelector("#todo-title")
 const cate = document.querySelector("#todo-category")
+const button = document.querySelector("#button-add")
 
 form.addEventListener("submit", event => {
     event.preventDefault()
+
+    button.setAttribute("disabled", "disabled");
     API.put("todos", {
         title: todo.value,
         category: cate.value
-    }).then(data => {
-        console.log(data)
-        showTodos()
     })
+    .then(data => {
+        console.log(data)
+        console.log(data.error)
+        if (!data.error) {
+            showRecentTodos()
+        }
+    })
+    .finally(() => {
+        button.removeAttribute("disabled");
+    });
 })
+
+showRecentTodos()
