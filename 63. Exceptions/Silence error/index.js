a12fbfd74f26796f14d36f230b7ac434907342a1
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-20 21:15:05 -0800

    Create index.js

---
+++ 63. Exceptions/Silence error/index.js
@@ -0,0 +1,9 @@
+const button = document.querySelector("#remove-title");
+button.addEventListener("click", () => {
+    try {
+        document.querySelector("h1").remove();
+    } catch (error) {
+        console.error(error)
+    }
+});
+
