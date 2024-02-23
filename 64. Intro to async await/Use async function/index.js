607165e0f0ff1a21d6030e67b03992a030e28251
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-22 21:44:23 -0800

    Create index.js

---
+++ 64. Intro to async await/Use async function/index.js
@@ -0,0 +1,9 @@
+// do NOT modify the function
+const getMinimumVotingAge = async () => {
+    return 18;
+}
+
+// Sample usage. TODO: write it yourself
+getMinimumVotingAge().then(value => {
+    console.log(value);
+})
